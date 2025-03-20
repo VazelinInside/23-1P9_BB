ac75776d9c2c30a547ba873188489d8680c915a1
VazelinInside <vi4733425@gmail.com>
2025-03-20 09:42:02 +0300

    Создание базы данных, создание классов пользователя, роли и их связь

---
+++ models.py
@@ -0,0 +1,36 @@
+'''zxc'''
+from peewee import MySQLDatabase, Model, CharField, ForeignKeyField
+
+mysql_db = MySQLDatabase('db_bb', user='root', password='1234',
+                         host='127.0.0.1', port=3306)
+
+
+class BaseModel(Model):
+    '''zxc'''
+    class Meta:
+        '''zxc'''
+        database = mysql_db
+
+
+class User(BaseModel):
+    '''zxc'''
+    username = CharField()
+    name = CharField()
+    password = CharField()
+    link_tg = CharField()
+
+
+class Role(BaseModel):
+    '''zxc'''
+    name = CharField()
+
+
+class UserRole(BaseModel):
+    '''zxc'''
+    user = ForeignKeyField(User)
+    role = ForeignKeyField(Role)
+
+
+if __name__ == '__main__':
+    mysql_db.connect()
+    mysql_db.create_tables([User, Role, UserRole])
